965a05617f34d10332c74391b488bf97d6d5aa71
Shekhar Prasad Rajak <shekharrajak@live.com>
2016-07-20 14:08:29 +0530

    ascii print
    ascii print
    
    ascii print
    
    ascii print

--- lib/app.rb
+++ lib/app.rb
@@ -6,26 +6,103 @@ def setup_files
     path = File.join(File.dirname(__FILE__), '../data/products.json')
     file = File.read(path)
     $products_hash = JSON.parse(file)
+    $report_file = File.new("../report.txt", "w+")
+end
+
+def print (output)
+  $report_file.puts output
 end
 
 def print_date
   time = Time.new
   line = "-- Date \n" + "today's date : " + time.inspect + "\n\n"
-  print_in_file(line)
+  print(line)
 end
 
-def print_in_file(line)
-  File.open("../report.txt", "a") do |file|
-    file.puts line
-  end
+# Print "Products" in ascii art
+def print_products_header
+  $report_file.puts "                     _            _       "
+  $report_file.puts "                    | |          | |      "
+  $report_file.puts " _ __  _ __ ___   __| |_   _  ___| |_ ___ "
+  $report_file.puts "| '_ \\| '__/ _ \\ / _` | | | |/ __| __/ __|"
+  $report_file.puts "| |_) | | | (_) | (_| | |_| | (__| |_\\__ \\"
+  $report_file.puts "| .__/|_|  \\___/ \\__,_|\\__,_|\\___|\\__|___/"
+  $report_file.puts "| |                                       "
+  $report_file.puts "|_|                                       "
+  $report_file.puts
+  $report_file.puts
 end
 
 # Print "Sales Report" in ascii art
-def method_name
+def print_sales_report_header
+  $report_file.puts "  _____       _             _____                       _    "
+  $report_file.puts " / ____|     | |           |  __ \\                     | |   "
+  $report_file.puts "| (___   __ _| | ___  ___  | |__) |___ _ __   ___  _ __| |_  "
+  $report_file.puts " \\___ \\ / _` | |/ _ \\/ __| |  _  // _ \\ '_ \\ / _ \\| '__| __| "
+  $report_file.puts " ____) | (_| | |  __/\\__ \\ | | \\ \\  __/ |_) | (_) | |  | |_  "
+  $report_file.puts "|_____/ \\__,_|_|\\___||___/ |_|  \\_\\___| .__/ \\___/|_|   \\__| "
+  $report_file.puts "                                      | |                    "
+  $report_file.puts "                                      |_|                    "
+  $report_file.puts
+  $report_file.puts
+end
+
+# Print "Brands" in ascii art
+def print_brands_header
+  $report_file.puts " _                         _     "
+  $report_file.puts "| |                       | |    "
+  $report_file.puts "| |__  _ __ __ _ _ __   __| |___ "
+  $report_file.puts "| '_ \\| '__/ _` | '_ \\ / _` / __|"
+  $report_file.puts "| |_) | | | (_| | | | | (_| \\__ \\"
+  $report_file.puts "|_.__/|_|  \\__,_|_| |_|\\__,_|___/"
+  $report_file.puts
+  $report_file.puts
+end
+
+def product_report
+
+  def amount_sale(item)
+    amount = item["purchases"].inject(0) { |amt, purchase| amt + purchase["price"] }
+    return amount
+  end
+
+  print_sales_report_header
+  print_products_header
+
+  $products_hash["items"].each do |_item|
+    print("title : " + _item["title"])
+    print("price : " + _item["price"])
+    print("purchases : " + _item["purchases"].size)
+    print("amount of sales : " + amount_sale(_item))
+
+  end
+end
+
+def brand_report
+  print_brands_header
 
 end
+
 def create_report
+    # Print today's date
     print_date
+
+    # Products report
+    # For each product in the data set:
+    # Print the name of the toy
+    # Print the retail price of the toy
+    # Calculate and print the total number of purchases
+    # Calculate and print the total amount of sales
+    # Calculate and print the average price the toy sold for
+    # Calculate and print the average discount (% or $) based off the average sales price
+    product_report
+
+    # For each brand in the data set:
+    # Print the name of the brand
+    # Count and print the number of the brand's toys we stock
+    # Calculate and print the average price of the brand's toys
+    # Calculate and print the total sales volume of all the brand's toys combined
+    brand_report
 end
 
 def start
@@ -34,25 +111,3 @@ def start
 end
 
 start # call start method to trigger report generation
-
-
-
-# Print today's date
-
-# Print "Products" in ascii art
-
-# For each product in the data set:
-	# Print the name of the toy
-	# Print the retail price of the toy
-	# Calculate and print the total number of purchases
-	# Calculate and print the total amount of sales
-	# Calculate and print the average price the toy sold for
-	# Calculate and print the average discount (% or $) based off the average sales price
-
-# Print "Brands" in ascii art
-
-# For each brand in the data set:
-	# Print the name of the brand
-	# Count and print the number of the brand's toys we stock
-	# Calculate and print the average price of the brand's toys
-	# Calculate and print the total sales volume of all the brand's toys combined
